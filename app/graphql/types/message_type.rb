1d4f6ae30103adfc46baa85896f002df50385b8f
DashaGerasimova <dashagera@gmail.com>
2017-11-21 16:51:17 +0300

    Created gql type and mutations for message model

---
+++ app/graphql/types/message_type.rb
@@ -0,0 +1,6 @@
+Types::MessageType = GraphQL::ObjectType.define do
+  name 'Message'
+
+  field :id, !types.ID
+  field :text, !types.String
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,10 +2,5 @@ Types::MutationType = GraphQL::ObjectType.define do
   name "Mutation"
 
   # TODO: Remove me
-  field :testField, types.String do
-    description "An example field added by the generator"
-    resolve ->(obj, args, ctx) {
-      "Hello World!"
-    }
-  end
+  field :create_message, function: Resolvers::CreateMessage.new
 end
